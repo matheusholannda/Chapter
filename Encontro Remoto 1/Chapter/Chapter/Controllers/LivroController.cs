22f78190f3c714ab4a45470db5b05a3fcc4e8c7b
matheus.holanda <matheus.holanda@info.local>
2023-01-31 13:36:30 -0300

    Conteúdo desenvolvido no encontro remoto 1

---
+++ Encontro Remoto 1/Chapter/Chapter/Controllers/LivroController.cs
@@ -0,0 +1,33 @@
+﻿using Chapter.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.Controllers
+{
+    [Produces("application/json")]//formato de reposta da api : json
+    [Route("api/[controller]")]//rota para acesso do controller : api/livro
+    [ApiController]//identificação que é uma classe controladora
+    public class LivroController : ControllerBase//herança da classe ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;//variável privada criada para armazenar os dados do repositório
+
+        public LivroController(LivroRepository livroRepository)//injeção de dependência: o controller depende do repository
+        {
+            _livroRepository = livroRepository;//armazenamento das informações do repositório dentro da variável privada
+        }
+
+        [HttpGet]//verbo Get: de obter
+        //IActionResult : https://learn.microsoft.com/pt-br/aspnet/core/web-api/action-return-types?view=aspnetcore-7.0
+        public IActionResult Listar()//nome do controller para listar os livros 
+        {
+            try//caso dê certo
+            {
+                return Ok(_livroRepository.Ler());//retorna um status code 200 e os itens da lista
+            }
+            catch (Exception e)//caso dê errado
+            {
+                throw new Exception(e.Message);//retorna uma mensagem de erro
+            }
+        } 
+    }
+}
---
+++ Encontro Remoto 1/Chapter/Chapter/Interfaces/ILivroRepository.cs
@@ -0,0 +1,12 @@
+﻿using Chapter.Models;
+
+namespace Chapter.Interfaces
+{
+    /// <summary>
+    /// Interface ILivroRepository
+    /// </summary>
+    public interface ILivroRepository
+    {
+        List<Livro> Ler();//método que deverá ser implementado pela classe que herdar desta interface
+    }
+}
