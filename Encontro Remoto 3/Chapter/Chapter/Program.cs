dc6405038e8c18f917682f31d0b33abe6d90b67d
matheus.holanda <matheus.holanda@info.local>
2023-02-14 18:06:21 -0300

    Conteúdo desenvolvido no encontro remoto 3

---
+++ Encontro Remoto 3/Chapter/Chapter/Program.cs
@@ -0,0 +1,78 @@
+using Chapter.Contexts;
+using Chapter.Interfaces;
+using Chapter.Repositories;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<ChapterContext, ChapterContext>();
+builder.Services.AddTransient<ILivroRepository, LivroRepository>();
+builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+
+builder.Services.AddControllers();
+
+// Add services CORS
+builder.Services.AddCors(options =>
+{
+
+    options.AddPolicy("CorsPolicy", builder =>
+
+    {
+
+        builder.WithOrigins("http://localhost:3000")
+
+        .AllowAnyHeader()
+
+        .AllowAnyMethod();
+    });
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+// Add services documentation Swagger 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Chapter API",
+        Description = "API de livros desenvolvida na UC de Desenvolvimento de API",        
+        Contact = new OpenApiContact
+        {
+            Name = "Carlos Roque",            
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Chapter License"           
+        }
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
